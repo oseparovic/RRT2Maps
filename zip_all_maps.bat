c6d80342eb0a2b80b25d1472707aff01446107bb
oseparovic <oseparovic@gmail.com>
2013-10-22 11:10:48 -0700

    Update zip_all_maps.bat

--- zip_all_maps.bat
+++ zip_all_maps.bat
@@ -1,1 +1,3 @@
 REM Still to be implemented! This will zip up all map files *.map and *.mp2 files into a single zip file on Windows machines. Simply double click to run.
+REM http://superuser.com/questions/110991/can-you-zip-a-file-from-the-command-prompt-using-only-windows-built-in-capabili
+REM http://stackoverflow.com/questions/1645843/resolve-absolute-path-from-relative-path-and-or-file-name
