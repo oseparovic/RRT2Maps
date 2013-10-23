5dd2db23248742a4fe9fa1a82b52ce23269f87d0
oseparovic <oseparovic@gmail.com>
2013-10-22 20:39:46 -0700

    Renamed a few files with special characters, created batch script for compiling all maps

--- zip_all_maps.bat
+++ zip_all_maps.bat
@@ -1,3 +1,4 @@
-REM Still to be implemented! This will zip up all map files *.map and *.mp2 files into a single zip file on Windows machines. Simply double click to run.
-REM http://superuser.com/questions/110991/can-you-zip-a-file-from-the-command-prompt-using-only-windows-built-in-capabili
-REM http://stackoverflow.com/questions/1645843/resolve-absolute-path-from-relative-path-and-or-file-name
+mkdir AllMaps
+for /r Maps %%f in (*.map) do @copy %%f AllMaps
+for /r Maps %%f in (*.mp2) do @copy %%f AllMaps
+PAUSE
